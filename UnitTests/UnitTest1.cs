60acf23f9787e799d20536d63329d5b0f18be395
Archer <archer.15@mail.ru>
2021-01-16 11:35:48 +0200

    //tesing

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -1,18 +1,42 @@
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace UnitTests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void Test1Andriuha()
         {
+            var stringOne = "Fedea";
+            var stringTwo = "Andriuha";
+
+            if (stringOne == stringTwo)
+                Assert.Fail();
+            else
+                Assert.Pass();
         }
 
         [Test]
-        public void Test1()
+        public void Test2Andriuha()
         {
-            Assert.Pass();
+            var stringOne = "Fedea";
+            var stringTwo = "Fedea";
+
+            if (stringOne == stringTwo)
+                Assert.Fail();
+            else
+                Assert.Pass();
+        }
+
+        [Test]
+        public void Test3Andriuha()
+        {
+            var arrayOne = new int[] { 1, 2, 4, 6, 7 };
+
+            for (int i = 0; i < arrayOne.Length; i++)
+                if (arrayOne[i] == 1)
+                    Assert.Pass();
+            Assert.Fail();
         }
     }
 }
